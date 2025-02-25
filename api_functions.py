a83ba45ce3a8cfdab51bd09cf39798c20fb42e1f
german-alvarez-dev <german.alvarez@ironhack.com>
2025-02-25 19:59:43 +0100

    data formatting functions

---
+++ api_functions.py
@@ -0,0 +1,46 @@
+import requests
+
+def get_IMDB_rating(ratings):
+
+    imdb_rating = None
+
+    for rating in ratings:
+        if rating["Source"] == "Internet Movie Database":
+            imdb_rating = rating["Value"]
+            break
+
+    return imdb_rating if imdb_rating else "N/A"
+
+
+
+def add_movie_ratings(movies_df):
+
+    imdb_ratings = []
+
+    for index, row in movies_df.iterrows():
+        movie_id = row["movie_id"]
+        params = {
+            "apikey": "e89f7fbc" ,
+            "i": movie_id 
+        }
+            
+        url = "https://www.omdbapi.com"
+
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+            ratings = data.get("Ratings", [])
+            imdb_rating = get_IMDB_rating(ratings)
+            imdb_ratings.append(imdb_rating)
+        else:
+            print(f"Error para la película {movie_id}: {response.status_code}")
+            imdb_ratings.append("Error")
+
+    movies_df["imdb_rating"] = imdb_ratings
+    return movies_df
+
+
+# rating_df = add_movie_ratings(df)
+
+# print(rating_df)
